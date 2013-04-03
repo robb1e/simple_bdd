ea61621cc3d61d2b608eac0a55c8cef5c5fd00ce
Adam Berlin <zephyr-dev@googlegroups.com>
2013-04-03 15:26:48 -0400

    Add 'Also' and 'also' as terms to the SimpleBDD language.

--- spec/simple_bdd_spec.rb
+++ spec/simple_bdd_spec.rb
@@ -5,11 +5,10 @@ class SimpleBddExample
 end
 
 describe SimpleBddExample do
-
   let(:subject) { SimpleBddExample.new }
 
-  describe "#given, #when, #then, #and" do
-    ["given", "when", "then", "and", "Given", "When", "Then", "And"].each do |method|
+  describe "#given, #when, #then, #and, #also" do
+    ["given", "when", "then", "and", "Given", "When", "Then", "And", "Also", "also"].each do |method|
       it "calls the method after translating the string" do
         subject.should_receive(:something_something_darkside)
         subject.send(method, "Something, Something Darkside")
