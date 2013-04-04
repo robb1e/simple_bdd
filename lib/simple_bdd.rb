d9f104e9c12b6ac469936761c4da7d8aefb9f9f5
Adam Berlin and Ryan McGarvey <pair+adam+ryan@pivotallabs.com>
2013-04-04 18:33:28 -0400

    A quick reduction of duplication.

--- lib/simple_bdd.rb
+++ lib/simple_bdd.rb
@@ -3,10 +3,11 @@ require "simple_bdd/version"
 module SimpleBdd
   %w[Given When Then And Also].each do |method|
     define_method(method) do |message|
+      translated_message = translate_message(message)
       begin
-        send translate_message(message)
+        send translated_message
       rescue NoMethodError
-        STDOUT.puts("def #{translate_message(message)}\n\nend")
+        STDOUT.puts("def #{translated_message}\n\nend")
         fail
       end
     end
