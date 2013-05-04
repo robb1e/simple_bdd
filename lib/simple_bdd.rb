612826fbdc140295a9d9f57349ab4cf0e140ac06
Dan Finnie <dan@danfinnie.com>
2013-05-04 15:10:53 -0400

    Extract methodize(str) method and test separately.

--- lib/simple_bdd.rb
+++ lib/simple_bdd.rb
@@ -4,10 +4,16 @@ module SimpleBdd
 
   %w[Given When Then And].each do |method|
     define_method(method) do |message|
-      send message.downcase.gsub(" ", "_").gsub(/\W/, "")
+      send methodize(message)
     end
 
     alias_method method.downcase, method
+  end
+
+  private
+
+  def methodize str
+    str.downcase.gsub(" ", "_").gsub(/\W/, "")
   end
 
 end
