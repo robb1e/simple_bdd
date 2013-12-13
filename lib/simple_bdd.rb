8d70a582bc9344db82d69f02e3eb3ec91ab5e966
Nikolay Sturm <sturm@erisiandiscord.de>
2013-12-13 07:30:52 +0100

    make behaviour on missing step implementation configurable
    Introduces RSpec option raise_error_on_missing_step_implementation,
    which is false by default. If true, a custom error will be raised when a
    step implementation is missing, otherwise the spec will be marked pending.

--- lib/simple_bdd.rb
+++ lib/simple_bdd.rb
@@ -1,13 +1,22 @@
 require "simple_bdd/version"
 
 module SimpleBdd
+  class StepNotImplemented < StandardError; end
+
+  RSpec.configuration.add_setting(:raise_error_on_missing_step_implementation,
+                                  default: false) if defined?(::RSpec)
+
   %w[Given When Then And Also But].each do |method|
     define_method(method) do |message|
       method_name = methodize(message)
       if respond_to? method_name || !defined?(::RSpec)
         send method_name
       else
-        pending(method_name)
+        if RSpec.configuration.raise_error_on_missing_step_implementation?
+          raise StepNotImplemented, method_name
+        else
+          pending(method_name)
+        end
       end
     end
 
